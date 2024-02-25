7dfb27bd98694c6e878ef758f3a5c6e9c25b2ddd
David Jordan <le_dangereux@hotmail.co.uk>
2024-02-25 22:42:58 +0000

    Constrain length of snake displayed on board

--- src/sneck/classes/game.py
+++ src/sneck/classes/game.py
@@ -7,13 +7,15 @@ from .snake import Snake
 
 
 class Game:
-    def __init__(self, fps=7):
-        self._frame_duration = 1.0 / 7
+    game_counter = 1
+
+    def __init__(self, fps=1):
+        self._frame_duration = 1.0 / fps
         self.stdscr = curses.initscr()
         self.configure_curses()
 
         self._board = Board()
-        self._snake = Snake(position=self._board.get_center())
+        self._snake = Snake(initial_position=self._board.get_center())
 
     def configure_curses(self):
         curses.noecho()
@@ -21,18 +23,20 @@ class Game:
         self.stdscr.nodelay(True)
 
     def run(self):
+        self.draw_snake_head()
         while True:
             self.stdscr.clear()
-            self.process_user_input()
-            self._snake.move()
-            self.add_snake_to_board()
+            self.remove_old_tail()
+            self.draw_snake_head()
             self.display_board()
+            self._snake.move()
             time.sleep(self._frame_duration)
+            self.process_user_input()
+            self.game_counter += 1
 
     def display_board(self):
         # TODO: Protect against exceptions due to the terminal being too small
         # to draw the full string
-        # TODO: Figure out why this is flickering
         self.stdscr.addstr(str(self._board))
         self.stdscr.refresh()
 
@@ -55,8 +59,12 @@ class Game:
             case "l":
                 self._snake.set_direction(Direction.RIGHT)
 
-    def add_snake_to_board(self):
-        position = self._snake.get_position()
-        head = self._snake.head
+    def remove_old_tail(self):
+        if self.game_counter > self._snake.get_length():
+            old_tail_position = self._snake.get_old_tail_position()
+            self._board.write_cell(old_tail_position, " ")
 
-        self._board.write_cell(position, head)
+    def draw_snake_head(self):
+        head_position = self._snake.get_head_position()
+        head = self._snake.head
+        self._board.write_cell(head_position, head)
--- src/sneck/classes/snake.py
+++ src/sneck/classes/snake.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 from ..assets.ascii_chars import snake_chars
 from .direction import Direction
 from .position import Position
@@ -6,24 +8,43 @@ from .position import Position
 class Snake:
     head = snake_chars["head"]
 
-    def __init__(self, position: Position, direction: Direction = Direction.UP):
-        print(self.head)
-        self._position = position
+    def __init__(
+        self,
+        initial_position: Position,
+        direction: Direction = Direction.UP,
+        length: int = 3,
+    ):
         self._direction = direction
+        self._body_positions = [initial_position]
+        self._length = length if length > 0 else 1
+        self._old_tail_position = initial_position
 
-    def get_position(self):
-        return self._position
+    def get_head_position(self):
+        return copy(self._body_positions[-1])
+
+    def get_old_tail_position(self) -> Position:
+        return self._old_tail_position
+
+    def get_length(self) -> int:
+        return self._length
 
     def set_direction(self, direction: Direction):
+        # TODO: Prevent backtracking
         self._direction = direction
 
     def move(self):
+        current_position = self.get_head_position()
+
         match self._direction:
             case Direction.UP:
-                self._position.row -= 1
+                current_position.row -= 1
             case Direction.LEFT:
-                self._position.col -= 1
+                current_position.col -= 1
             case Direction.RIGHT:
-                self._position.col += 1
+                current_position.col += 1
             case Direction.DOWN:
-                self._position.row += 1
+                current_position.row += 1
+
+        self._body_positions.append(current_position)
+        if len(self._body_positions) > self._length:
+            self._old_tail_position = self._body_positions.pop(0)
