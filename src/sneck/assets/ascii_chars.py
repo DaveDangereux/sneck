884065fa88614a88e59741b175354664a2c25155
David Jordan <le_dangereux@hotmail.co.uk>
2024-02-26 23:49:05 +0000

    Thicken box characters

--- src/sneck/assets/ascii_chars.py
+++ src/sneck/assets/ascii_chars.py
@@ -1,10 +1,10 @@
 box_chars = {
-    "top_left": "╭",
-    "top_right": "╮",
-    "bottom_left": "╰",
-    "bottom_right": "╯",
-    "horizontal_bar": "―",
-    "vertical_bar": "│",
+    "top_left": "╔",
+    "top_right": "╗",
+    "bottom_left": "╚",
+    "bottom_right": "╝",
+    "horizontal_bar": "═",
+    "vertical_bar": "║",
 }
 
 snake_chars = {"head": "█"}
